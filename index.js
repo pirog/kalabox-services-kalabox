0a960e6c0abcb119dc67e9b524958efe89b72b20
Mike Pirog <mike@kalamuna.com>
2015-04-08 22:30:08 -0700

    #364: first pass on update -> provision

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 module.exports = function(kbox) {
 
   require('./lib/events.js')(kbox);
-  require('./lib/install.js')(kbox);
-  require('./lib/updates.js')(kbox);
+  //require('./lib/install.js')(kbox);
 
 };
