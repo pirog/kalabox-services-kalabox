38c70890b26faf19d2ad1ff08971e104fe9d9117
Mike Pirog <mike@kalamuna.com>
2015-07-06 17:45:38 -0700

    #285: Switch to shallow hipache fork

--- dockerfiles/hipache/config/keys.sh
+++ dockerfiles/hipache/config/keys.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
 # Set up our certs
-if [ ! -f "/certs/binding.pem" ]; then
-  openssl genrsa -out /certs/binding.key 2048 && \
-  openssl req -new -x509 -key /certs/binding.key -out /certs/binding.crt -days 365 -subj "/C=US/ST=California/L=Oakland/O=Kalabox/OU=KB/CN=*.${DOMAIN}" && \
-  cat /certs/binding.crt /certs/binding.key > /certs/binding.pem && \
-  mkdir /usr/share/ca-certificates/*.${DOMAIN} && \
-  cp /certs/binding.crt /usr/share/ca-certificates/*.${DOMAIN}/binding.crt && \
-  echo "*.${DOMAIN}/binding.crt" >> /etc/ca-certificates.conf && \
-  update-ca-certificates --fresh
+if [ ! -f "/certs/hipache.pem" ]; then
+  openssl genrsa -out /certs/hipache.key 2048 && \
+  openssl req -new -x509 -key /certs/hipache.key -out /certs/hipache.crt -days 365 -subj "/C=US/ST=California/L=Oakland/O=Kalabox/OU=KB/CN=*.${DOMAIN}" && \
+  cat /certs/hipache.crt /certs/hipache.key > /certs/hipache.pem
 fi
